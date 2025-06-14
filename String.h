e0135a877db443df026cd3d0cdacc32acb9e2dde
Zard delas <aparecidohallyson@gmail.com>
2025-06-14 12:29:12 -0300

    define headers

--- String.h
+++ String.h
@@ -2,7 +2,13 @@
 #define STRING_H
 #include <stddef.h>
 
+typedef struct 
+{
+    char * data;
+    size_t length;
+}String;
 
-
-
+String createString(const char * data);
+void printString(String * str);
+void freeMemory(String * str);
 #endif
